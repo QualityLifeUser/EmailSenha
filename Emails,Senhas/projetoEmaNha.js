e49f14a648d128b9210209706d1678fb1f5bc0b2
QualityLifeUser <145879411+QualityLifeUser@users.noreply.github.com>
2023-09-23 23:42:03 -0300

    Add files via upload

---
+++ Emails,Senhas/projetoEmaNha.js
@@ -0,0 +1,15 @@
+// email youtube: healthlifeuser@gmail.com
+//  senha : jonaspedroj0n4sp3dr0123456
+
+
+// email tiktok: qualityforlifeUser@gmail.com
+// senha: jonaspedroj0n4sp3dr0123456
+
+
+// email github: githubqualitylife@gmail.com
+// senha: jonaspedroj0n4sp3dr0123456
+
+
+// Atualizar as senhas caso sejam trocas e adicionar os emails e senhas novos que forem criados
+
+// sucesso pra nos!!
